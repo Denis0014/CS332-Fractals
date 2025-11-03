888fadb4745b97ce6ddabbbe7d70bda14ad8a6ab
Denis <109104246+Denis0014@users.noreply.github.com>
2025-11-03 15:17:41 +0300

    Update fractals.py

--- fractals.py
+++ fractals.py
@@ -1,7 +1,7 @@
 import re
 import tkinter
 import numpy as np
-from typing import Union
+from typing import Union, Any, Sequence
 
 WIDTH = 300
 HEIGHT = 300
@@ -10,12 +10,15 @@ Shape = Union["Point", "Line"]
 Number = Union[int, float]
 
 
-class Point:
+class BasePoint:
     def __init__(self, x: Number, y: Number) -> None:
         self.x = x
         self.y = y
 
     def __repr__(self) -> str:
+        features = ", ".join(f"{k}={repr(v)}" for k, v in self.__dict__.items())
+        if features:
+            return f"{self.__class__.__name__}({self.x}, {self.y}, {features})"
         return f"{self.__class__.__name__}({self.x}, {self.y})"
 
     def __eq__(self, other: object) -> bool:
@@ -30,8 +33,26 @@ class Point:
         return hash((self.x, self.y))
 
 
-class Line:
-    def __init__(self, start: Point, end: Point) -> None:
+class Point(BasePoint):
+    def __init__(self, x: Number, y: Number) -> None:
+        super().__init__(x, y)
+        self.__features = dict[str, Any]()
+
+    def __getitem__(self, item: str) -> Any:
+        return self.__features.get(item, None)
+    
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.__features[key] = value
+
+    def __repr__(self) -> str:
+        features = ", ".join(f"{k}={repr(v)}" for k, v in self.__features.items())
+        if features:
+            return f"{self.__class__.__name__}({self.x}, {self.y}, {features})"
+        return f"{self.__class__.__name__}({self.x}, {self.y})"
+
+
+class BaseLine:
+    def __init__(self, start: BasePoint, end: BasePoint) -> None:
         self.start = start
         self.end = end
 
@@ -39,7 +60,7 @@ class Line:
         return f"{self.__class__.__name__}({self.start}, {self.end})"
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Line):
+        if not isinstance(other, BaseLine):
             return False
         return self.start == other.start and self.end == other.end
 
@@ -48,6 +69,24 @@ class Line:
 
     def __hash__(self) -> int:
         return hash((self.start, self.end))
+
+
+class Line(BaseLine):
+    def __init__(self, start: BasePoint, end: BasePoint) -> None:
+        super().__init__(start, end)
+        self.__features = dict[str, Any]()
+
+    def __getitem__(self, item: str) -> Any:
+        return self.__features.get(item, None)
+    
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.__features[key] = value
+
+    def __repr__(self) -> str:
+        features = ", ".join(f"{k}={repr(v)}" for k, v in self.__features.items())
+        if features:
+            return f"{self.__class__.__name__}({self.start}, {self.end}, {features})"
+        return f"{self.__class__.__name__}({self.start}, {self.end})"
 
 
 class BaseCanvas:
@@ -126,11 +165,11 @@ class TkinterCanvas(BaseCanvas):
         )
         self.tk_canvas.pack()
 
-    def make_points(self, points: list[Point]) -> None:
+    def make_points(self, points: Sequence[Point]) -> None:
         for point in points:
             self += point
 
-    def make_lines(self, lines: list[Line]) -> None:
+    def make_lines(self, lines: Sequence[Line]) -> None:
         for line in lines:
             if line.start == line.end:
                 continue
@@ -158,7 +197,8 @@ class TkinterCanvas(BaseCanvas):
 
             point1 = Point(x0, y0)
             point2 = Point(x1, y1)
-            line = Line(point1, point2)
+            line.start = point1
+            line.end = point2
 
             self.lines[i] = line
             self += point1
@@ -180,15 +220,15 @@ class Fractal:
         string_rules = rules
         self.rules: dict[str, list[str]] = {}
 
-        self.atom = self.__parse_rule(self.atom)
+        self.atom = self.parse_rule(self.atom)
 
         for key, value in string_rules.items():
-            self.rules[key] = self.__parse_rule(value)
+            self.rules[key] = self.parse_rule(value)
 
-    def __parse_rule(self, rule: str) -> list[str]:
+    def parse_rule(self, rule: str) -> list[str]:
         parsed_rule = list[str]()
         for char in rule:
-            if char not in self.rules and re.match(r"[^A-Z\+\-\[\]]", char):
+            if char not in self.rules and re.match(r"[^A-Z\+\-\[\]\*\<\>]", char):
                 raise ValueError("Invalid character in rule: " + char)
             parsed_rule.append(char)
         return parsed_rule
@@ -213,11 +253,92 @@ class Fractal:
 
 
 if __name__ == "__main__":
+    ITERATIONS = 9
+
+    primary_color = "#111111"
+    secondary_color = "#39dd57"
+
+
+    def hex_to_rgb(hex_color: str) -> tuple[int, int, int]:
+        hex_color = hex_color.lstrip("#")
+        return (
+            int(hex_color[0:2], 16),
+            int(hex_color[2:4], 16),
+            int(hex_color[4:6], 16),
+        )
+    
+
+    def gradient_generator(color1: str, color2: str, steps: int):
+        r1, g1, b1 = hex_to_rgb(color1)
+        r2, g2, b2 = hex_to_rgb(color2)
+
+        for step in range(steps):
+            t = step / (steps - 1)
+            r = int(r1 + (r2 - r1) * t)
+            g = int(g1 + (g2 - g1) * t)
+            b = int(b1 + (b2 - b1) * t)
+            yield f"#{r:02x}{g:02x}{b:02x}"
+
+
+    def apply_rules(frac: Fractal, frac_rules: Sequence[str], random_angle: float = 0.0) -> tuple[list[Point], list[Line]]:
+        current_point = Point(0, 0)
+        lines = list[Line]()
+        points = [current_point]
+        stack = list[tuple[Number, Point, int, int]]()
+        gradient_colors = list(gradient_generator(primary_color, secondary_color, ITERATIONS + 1))
+        current_color_index = 0
+        line_width = 1
+
+        for char in frac_rules:
+            if re.match(r"[A-Z]", char):
+                rad = np.radians(frac.rotation)
+                new_x = current_point.x + line_length * np.cos(rad)
+                new_y = current_point.y + line_length * np.sin(rad)
+                new_point = Point(new_x, new_y)
+                lines.append(Line(current_point, new_point))
+                lines[-1]['color'] = gradient_colors[current_color_index]
+                lines[-1]['width'] = line_width
+                current_point = new_point
+                points.append(current_point)
+
+            elif char == "+":
+                if random_angle:
+                    frac.rotate(frac.angle + np.random.uniform(random_angle * -1, random_angle))
+                else:
+                    frac.rotate(frac.angle)
+
+            elif char == "-":
+                if random_angle:
+                    frac.rotate(-frac.angle + np.random.uniform(random_angle * -1, random_angle))
+                else:
+                    frac.rotate(-frac.angle)
+
+            elif char == "[":
+                stack.append((frac.rotation, current_point, line_width, current_color_index))
+
+            elif char == "]":
+                frac.rotation, current_point, line_width, current_color_index = stack.pop()
+
+            elif char == "*":
+                current_color_index = min(ITERATIONS, current_color_index + 1)
+
+            elif char == "<":
+                line_width = max(1, line_width - 1)
+
+            elif char == ">":
+                line_width += 1
+
+        return points, lines
+
+
     frac = Fractal(
-        atom="X",
-        angle=20,
-        start_rotation=270,
-        rules={"F": "FF", "X": "F[+X]F[-X]+X"}
+        atom="A",
+        angle=60,
+        start_rotation=0,
+        rules={
+            "A": "B-A-B",
+            "B": "A+B+A"
+        }
     )
     print(frac)
 
@@ -228,35 +349,14 @@ if __name__ == "__main__":
 
     print("Fractal iterations: ")
     # --- Generate fractal ---
-    for iteration in frac(6):
+    for iteration in frac(ITERATIONS):
         print(iteration, end=" ")
         frac_rules.append(iteration)
 
     print("\nDrawing fractal...")
     line_length = 1
 
-    current_point = Point(0, 0)
-    lines = list[Line]()
-    points = [current_point]
-    stack = list[tuple[Number, Point]]() # type: ignore
-
-    for command in frac_rules:
-        if re.match(r"[A-Z]", command):
-            rad = np.radians(frac.rotation)
-            new_x = current_point.x + line_length * np.cos(rad)
-            new_y = current_point.y + line_length * np.sin(rad)
-            new_point = Point(new_x, new_y)
-            lines.append(Line(current_point, new_point))
-            current_point = new_point
-            points.append(current_point)
-        elif command == "+":
-            frac.rotate(frac.angle)
-        elif command == "-":
-            frac.rotate(-frac.angle)
-        elif command == "[":
-            stack.append((frac.rotation, current_point))
-        elif command == "]":
-            frac.rotation, current_point = stack.pop()
+    points, lines = apply_rules(frac, frac_rules)
 
     center = Point(WIDTH // 2, HEIGHT // 2)
     frac_center = Point(0.0, 0.0)
@@ -271,8 +371,8 @@ if __name__ == "__main__":
     ymax = max(point.y for point in points)
     xmin = min(point.x for point in points)
     ymin = min(point.y for point in points)
-    scale_x = WIDTH / (xmax - xmin)
-    scale_y = HEIGHT / (ymax - ymin)
+    scale_x = WIDTH / dx if (dx := xmax - xmin) != 0 else 1
+    scale_y = HEIGHT / dy if (dy := ymax - ymin) != 0 else 1
     min_scale = min(scale_x, scale_y)
 
     matrix_scale = np.array(
@@ -299,7 +399,7 @@ if __name__ == "__main__":
 
     for line in canvas.lines:
         canvas.tk_canvas.create_line(
-            line.start.x, line.start.y, line.end.x, line.end.y, fill="black"
+            line.start.x, line.start.y, line.end.x, line.end.y, fill=line['color'], width=line['width']
         )
     # for point in canvas.points:
     #     canvas.tk_canvas.create_oval(
